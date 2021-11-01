625ff290780cac0c57dd423755b7d3e1204557a7
william lee <hey.williamlee@gmail.com>
2021-11-01 21:58:51 +0100

    add wslink to apollo client for using subscriptions

--- src/app/gql-client.ts
+++ src/app/gql-client.ts
@@ -1,4 +1,26 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+const wsLink = new WebSocketLink({
+  uri: process.env.REACT_APP_GRAPHQL_SUBSCRIPTION_API!,
+  options: {
+    reconnect: true,
+  },
+});
+
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_GRAPHQL_API,
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+);
 
 const cache = new InMemoryCache({
   addTypename: false,
@@ -6,7 +28,7 @@ const cache = new InMemoryCache({
 });
 
 export const GqlClient = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_API,
+  link: splitLink,
   cache,
   defaultOptions: {
     watchQuery: {
